9de597c55ec45f0aafbdd9ec3c4ca2d2983972a0
Gus Richardson <gus.richardson@icloud.com>
2017-03-17 13:36:45 -0400

    Added ajax

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 	
+	$("input[name='submit']").submit(function(){
+		.ajax(
+			URL = "https://en.wikipedia.org/w/api.php?" + "action=query&titles=" + encodeURIComponent($("input[name='submit']").val())+"&rvprop=content&prop=revisions"
+			
+			url: URL,
+			data: queryData,
+			dataType: 'json',
+			type: 'POST',
+			headers: { 'Api-User-Agent': 'Gus' },
+			success: function(data){
+					
+			};
+			
+			
+			
+		);
+	});
+	
 });
