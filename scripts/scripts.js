b1c7c49532529afa34de6c3855d70d604f315184
Gus Richardson <gus.richardson@icloud.com>
2017-03-17 14:28:28 -0400

    Removed jsonp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,7 @@
 $(document).ready(function(){
 	var search = $("input[name='submit']");
 	var results = [];
-	search.submit(function(){
-		
-			URL = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + encodeURIComponent($("input[name='submit']").val() + "&callback=JSON_CALLBACK";
-			
-			$http.jsonp(URL).success(function(data){
-				
-				results = data.query.pages;
-				
-				results.forEach(function(){
-					results.push([]);
-				});
-			});
-			
-			
-			
-		);
-	});
+	
+	
 	
 });
