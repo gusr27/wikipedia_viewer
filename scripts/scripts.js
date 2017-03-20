cbcb5add2e84170dfa58e5f2a465d5f44ee70bf2
Gus Richardson <gus.richardson@icloud.com>
2017-03-20 15:27:41 -0400

    Added functionality of API

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,95 @@
-$(document).ready(function(){
-	var search = $("#inputButton");
-	var results = [];
-	var query = '';
+var search = $("#inputButton");
 	
-	search.on("#submit", function(e){
+	var query = '';
+	var api = "https://en.wikipedia.org/w/api.php";
+	var cb = "&callback=JSON_CALLBACK";
+	
+	var dList= {
+			      "action": "query",
+			      "format": "json",
+			      "generator": "search",
+			      "gsrlimit": 10,
+			      "prop": "info|pageimages|extracts",
+			      "exintro": 1,
+			      "explaintext": 1,
+			      "exsentences": 1,
+			      "exlimit": "max",
+			      "pilimit": "max",
+			      "inprop": "url",
+			      "redirects": 1,
+			      "gsrsearch": query
+	};
+
+	
+
+
+function fetchApi(query){
+	var html = "";
+		$.ajax({
+				
+				
+				url: api,
+				dataType: 'json',
+				data: {
+			      "action": "query",
+			      "format": "json",
+			      "generator": "search",
+			      "gsrlimit": 10,
+			      "prop": "info|pageimages|extracts",
+			      "exintro": 1,
+			      "explaintext": 1,
+			      "exsentences": 1,
+			      "exlimit": "max",
+			      "pilimit": "max",
+			      "inprop": "url",
+			      "redirects": 1,
+			      "gsrsearch": query
+			    },
+			    type: 'POST',
+			    headers: {
+				  'Api-User-Agent': 'Example/1.0'  
+			    },
+				jsonp: 'callback',
+				async: false,
+				success: function(data){
+				
+					 var results = data.query.pages;
+					
+					$.each(results, function(index, value){
+						if (!value.thumbnail) {
+					      var image = " ";
+					    } else {
+					      var thumb = value.thumbnail.source;
+					      var image = "<img src='" + thumb + "' alt='Page image for " + value.title + "' class='thumbnail'>";
+					    }
+						
+						html+= "<li class=\"list-group-item\">" ;
+						html+= image;
+						html+= "<a href=" + value.canonicalurl + ">"+value.title + "</a>";
+						html+= "<p>" + value.extract + "</p>";
+						html+="</li>";
+					})
+					
+					$("#pages").html(html);
+					
+				}
+					
+				
+			});
+};
+
+$(document).ready(function(){
+	
+	search.on("submit", function(e){
 		
 		e.preventDefault();
-		query = $("#search").val().trim();
+		query = $("#searchQuery").val().trim();
 		
 		if ( query != ''){
-			console.log(query);
+			fetchApi(query);
 		}
 		else{
-			console.log("Nothing to display");
+			alert("Nothing to display");
 		};
 	});
 	
